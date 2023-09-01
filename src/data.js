6d50be1bf814025231d152598ae111b0e48428b7
Marco.Alberto <Marco.Alberto@gmail.com>
2023-08-31 21:12:08 -0600

    Taller1Ejemplo2

---
+++ src/data.js
@@ -0,0 +1,122 @@
+export const data=[
+    {
+        id:1,
+        title:'Café Mocca Helado',
+        price:4,
+        type: 'Bebida Fria',
+        urlImage:'https://www.vitamixespana.com/recetas/wp-content/uploads/2019/02/helado-de-vainilla-y-mocha-des.jpg',
+        quantity:1
+    },
+    {
+        id:2,
+        title:'Licor de Café y Brandy',
+        price:4,
+        type: 'Bebida Fria',
+        urlImage:'https://www.barexpres.com/wp-content/uploads/2012/12/carajillo-banner.jpg',
+        quantity:1
+    },
+    {
+        id:3,
+        title:'Café Capuchino Frío',
+        price:4,
+        type: 'Bebida Fria',
+        urlImage:'https://es.rc-cdn.community.thermomix.com/recipeimage/2dhvafen-1e8ca-630158-cfcd2-ma57h0tl/9a243c82-6f44-40fd-a673-9196ff1edb1b/main/capuchino-frio.jpg',
+        quantity:1
+    },
+    {
+        id:4,
+        title:'Café Espresso',
+        price:3,
+        type: 'Bebida Caliente',
+        urlImage:'https://www.recetasderechupete.com/wp-content/uploads/2021/04/cafe-expresso-1200x828.jpg',
+        quantity:1
+    }, 
+    {
+        id:5,
+        title:'Café Americano',
+        price:3,
+        type: 'Bebida Caliente',
+        urlImage:'https://bittercoffees.com/wp-content/uploads/2022/02/Cafe%CC%81-Americano-.jpeg',
+        quantity:1
+    },
+    {
+        id:6,
+        title:'Café Latte',
+        price:3,
+        type: 'Bebida Caliente',
+        urlImage:'https://previews.123rf.com/images/yktr/yktr1611/yktr161100007/68034748-una-taza-de-caf%C3%A9-latte.jpg',
+        quantity:1
+    },
+    {
+        id:7,
+        title:'Merengue de Café',
+        price:5,
+        type: 'Postre de Café',
+        urlImage:'https://www.hogarmania.com/archivos/201603/1830-merengue-de-cafe-y-mantequilla-1014-xl-1280x720x80xX.jpg',
+        quantity:1
+    },
+    {
+        id:8,
+        title:'Flan de Café',
+        price:5,
+        type: 'Postre de Café',
+        urlImage:'https://assets.unileversolutions.com/recipes-v2/237526.jpg',
+        quantity:1
+    },
+    {
+        id:9,
+        title:'Brazo Gitano de Café',
+        price:5,
+        type: 'Postres de Café',
+        urlImage:'https://mejorconsalud.as.com/wp-content/uploads/2015/03/brazo-gitano-cafe.jpg',
+        quantity:1
+    },
+    {
+        id:10,
+        title:'Té Negro',
+        price:2.5,
+        type: 'Te',
+        urlImage:'https://www.granoyhoja.cl/wp-content/uploads/2021/04/Te-Negro.jpg',
+        quantity:1
+    },
+    {
+        id:11,
+        title:'Té Chai Latte',
+        price:2.5,
+        type: 'Te',
+        urlImage:'https://www.splenda.com/wp-content/themes/bistrotheme/assets/recipe-images/chai-tea-blend.jpg',
+        quantity:1
+    },
+    {
+        id:12,
+        title:'Té de Jazmín',
+        price:2.5,
+        type: 'Te',
+        urlImage:'https://monkites.com/wp-content/uploads/portada-te-jazmin.jpg',
+        quantity:1
+    },
+    {
+        id:13,
+        title:'Sándwich Cubano',
+        price:3.7,
+        type: 'Sándwiches',
+        urlImage:'https://assets.elgourmet.com/wp-content/uploads/2023/03/cover_pv5iam8f1g_iStock-473336754.jpg',
+        quantity:1
+    },
+    {
+        id:14,
+        title:'Sándwich de Atún',
+        price:3.7,
+        type: 'Sándwiches',
+        urlImage:'https://www.pequerecetas.com/wp-content/uploads/2010/05/como-hacer-sandwich-de-atun.jpg',
+        quantity:1
+    },
+    {
+        id:15,
+        title:'Sándwich de Huevo',
+        price:3.7,
+        type: 'Sándwiches',
+        urlImage:'https://assets.kraftfoods.com/recipe_images/opendeploy/124292_640x428.jpg',
+        quantity:1
+    }
+];
